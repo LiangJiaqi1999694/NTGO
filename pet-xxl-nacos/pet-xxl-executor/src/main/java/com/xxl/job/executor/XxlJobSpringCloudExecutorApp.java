99731220b2660e42be1bb3c7cfec741044e53b56
zhuangyong <zhuangyong@hotwater.com.cn>
2023-04-14 14:24:19 +0800

    xxl

--- pet-xxl-nacos/pet-xxl-executor/src/main/java/com/xxl/job/executor/XxlJobSpringCloudExecutorApp.java
+++ pet-xxl-nacos/pet-xxl-executor/src/main/java/com/xxl/job/executor/XxlJobSpringCloudExecutorApp.java
@@ -3,13 +3,14 @@ package com.xxl.job.executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author majun
  * @description
  * @date 2020/5/12
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.xxl.job.executor","com.warrior.xxl"})
 @EnableDiscoveryClient
 public class XxlJobSpringCloudExecutorApp {
     public static void main(String[] args) {
